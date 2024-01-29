85a1a50c4016f875debcd637f1c587131f96590d
doinel1a <doinel1atanasiu@gmail.com>
2024-01-29 14:40:00 +0100

    refactor(footer): update copywright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import { Heart } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Footer() {
+  const website = 'https://www.defibuilder.com';
   const year = new Date().getFullYear();
 
   return (
     <footer className='flex h-10 w-full items-center justify-center border-t border-border text-sm'>
-      by &nbsp;
-      <Link href='https://business-link.d1a.app' className='text-primary'>
-        doinel1a
+      Made with &nbsp;
+      <Heart className='h-4 w-4 text-red-500' /> &nbsp; by
+      <Link
+        href={website}
+        className='px-1 transition-colors hover:text-primary focus-visible:text-primary'
+      >
+        DeFi Builder
       </Link>
-      &nbsp; {year}
+      &nbsp; &copy; &nbsp; {year}
     </footer>
   );
 }

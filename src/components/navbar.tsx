79016b367e04309e3f5756fb7b15f1eaf42fcf87
doinel1a <doinel1atanasiu@gmail.com>
2024-01-29 15:42:39 +0100

    refactor(navbar): update logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Image from 'next/image';
+
+import logo from '@/assets/images/logo.png';
+
 export default function Navbar() {
   return (
-    <header className='flex h-16 w-full items-center justify-between border-b border-border px-5'>
-      <span className='text-lg font-black'>Template</span>
+    <header className='flex h-20 w-full items-center justify-between border-b border-border px-5'>
+      <Image src={logo} alt="DeFi Builder's logo" width={190} height={30} />
     </header>
   );
 }

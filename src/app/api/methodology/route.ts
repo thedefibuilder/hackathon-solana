b9e7da28b89e8732113088050bb6b1222b787cb7
Alexe Spataru <alexeluca.spataru@gmail.com>
2024-01-30 15:32:02 +0200

    feat: try github api

--- src/app/api/methodology/route.ts
+++ src/app/api/methodology/route.ts
@@ -3,5 +3,10 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function POST(_request: NextRequest) {
-  return NextResponse.json({ output: 'Hello Brat!' }, { status: 200 });
+  try {
+    // TODO: Return list of vulnerabiltities that are searched
+    return NextResponse.json({ output: 'Hello Brat!' }, { status: 200 });
+  } catch (error: unknown) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
--- src/app/api/summary/route.ts
+++ src/app/api/summary/route.ts
@@ -1,7 +1,45 @@
 import type { NextRequest } from 'next/server';
 
 import { NextResponse } from 'next/server';
+import { Octokit } from 'octokit';
 
-export async function POST(_request: NextRequest) {
-  return NextResponse.json({ output: 'Hello Brat!' }, { status: 200 });
+import { db } from '@/server/db';
+
+export type TSummaryRequest = {
+  userId: string;
+  repoName: string;
+};
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = (await request.json()) as TSummaryRequest;
+    const userId = body.userId;
+
+    const account = await db.account.findFirstOrThrow({
+      where: {
+        userId
+      }
+    });
+    const octokit = new Octokit({
+      auth: account.access_token
+    });
+
+    const users = await octokit.rest.users.getAuthenticated();
+    console.log(users.data.login);
+
+    const response = await octokit.rest.repos.getContent({
+      owner: users.data.login,
+      repo: body.repoName,
+      path: '',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    // TODO: Add logic to parse the response and return the summary
+
+    return NextResponse.json({ output: response }, { status: 200 });
+  } catch (error: unknown) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }

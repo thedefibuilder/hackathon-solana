25139bf6874138a7afb7480fc7e44f6c449aec25
doinel1a <doinel1atanasiu@gmail.com>
2024-01-30 21:36:12 +0100

    refactor(next-auth): tried to save gh username into db on first log in - NOT WORKING

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -17,6 +17,57 @@ declare module 'next-auth' {
 
 export const authOptions: NextAuthOptions = {
   callbacks: {
+    /* IT IS TOO EARLY TO UPDATE THE ACCOUNT, AT THIS POINT IT IS NOT YET INSERTED IN THE DB
+    signIn: async ({ user, account, profile }) => {
+      if (user && account && profile) {
+        const _account = await db.account.findFirst({
+          where: {
+            userId: user.id
+          }
+        });
+        console.log('_account', _account);
+
+        if (!_account?.username) {
+          console.log('UPDATING WAIIII');
+          await db.account.update({
+            where: {
+              id: _account?.id
+            },
+            data: {
+              username: profile.name
+            }
+          });
+        }
+      }
+
+      return true;
+    }, */
+
+    /* IT NEVER GETS CALLED
+    jwt: async ({ user, account, profile, token }) => {
+      if (user && account && profile) {
+        const _account = await db.account.findFirst({
+          where: {
+            userId: user.id
+          }
+        });
+        console.log('_account', _account);
+
+        if (!_account?.username) {
+          console.log('UPDATING WAIIII');
+          await db.account.update({
+            where: {
+              id: _account?.id
+            },
+            data: {
+              username: profile.name
+            }
+          });
+        }
+      }
+
+      return token;
+    }, */
     session: ({ session, user }) => ({
       ...session,
       user: {
